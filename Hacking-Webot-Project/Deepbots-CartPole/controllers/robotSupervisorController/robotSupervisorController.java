283eb5f1b5e3ea0ed4d9b011eb64728d8a0f772e
Pusaka Manggala <pusakamanggala@gmail.com>
2023-01-20 14:43:07 +0700

    add : action space & eference to the robot node

--- Hacking-Webot-Project/Deepbots-CartPole/controllers/robotSupervisorController/robotSupervisorController.java
+++ Hacking-Webot-Project/Deepbots-CartPole/controllers/robotSupervisorController/robotSupervisorController.java
@@ -5,3 +5,23 @@ from PPO_agent import PPOAgent, Transition
 from gym.spaces import Box, Discrete
 import numpy as np
 
+
+class CartpoleRobot(RobotSupervisor):
+    def __init__(self):
+        super().__init__()
+        self.observation_space = Box(low=np.array([-0.4, -np.inf, -1.3, -np.inf]),
+                                     high=np.array([0.4, np.inf, 1.3, np.inf]),
+                                     dtype=np.float64)
+        self.action_space = Discrete(2)
+        
+        self.robot = self.getSelf()  # Grab the robot reference from the supervisor to access various robot methods
+        self.positionSensor = self.getDevice("polePosSensor")
+        self.positionSensor.enable(self.timestep)
+
+        self.poleEndpoint = self.getFromDef("POLE_ENDPOINT")
+        self.wheels = []
+        for wheelName in ['wheel1', 'wheel2', 'wheel3', 'wheel4']:
+            wheel = self.getDevice(wheelName)  # Get the wheel handle
+            wheel.setPosition(float('inf'))  # Set starting position
+            wheel.setVelocity(0.0)  # Zero out starting velocity
+            self.wheels.append(wheel)
