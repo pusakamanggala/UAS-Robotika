d27bb07ce22a318a469ad8f02eec7a13ac0debf5
Pusaka Manggala <pusakamanggala@gmail.com>
2023-01-21 14:40:40 +0700

    add : coding

--- Webots-Tutorials/Tutorial-1/controllers/epuck_go_forward/epuck_go_forward.py
+++ Webots-Tutorials/Tutorial-1/controllers/epuck_go_forward/epuck_go_forward.py
@@ -1,32 +1,16 @@
-"""epuck_go_forward controller."""
+from controller import Robot, Motor
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+TIME_STEP = 64
 
 # create the Robot instance.
 robot = Robot()
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+# get the motor devices
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+# set the target position of the motors
+leftMotor.setPosition(10.0)
+rightMotor.setPosition(10.0)
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
-
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+while robot.step(TIME_STEP) != -1:
+   pass
